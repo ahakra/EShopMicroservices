64926f3edb8f7f696a1d0f4e4caf261d3b0e8956
Ahmad Akra <ahakra@vanrise.com>
2025-05-27 16:13:39 +0300

    first catalogAPI

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+
+public record CreateProductRequest( Guid Id , string Name ,List<string> Category ,
+                                    string Description ,string ImageFile ,decimal Price );
+
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint
+{
+    
+}
