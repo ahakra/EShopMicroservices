c350e2e3bcec9f9d848e6c5cad3985a275e9e93d
Ahmad Akra <ahakra@outlook.com>
2025-05-30 06:50:17 +0300

    adding validation behavior

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,6 @@
 ﻿
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(
@@ -8,11 +10,26 @@ public record CreateProductCommand(
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session)
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor( x=>x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+        RuleFor(x=>x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+
+internal class CreateProductCommandHandler(
+        IDocumentSession session)
         :ICommandHandler<CreateProductCommand,CreateProductResult>
 {
+    
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        
         var product = new Product
         {
             Name = command.Name,
